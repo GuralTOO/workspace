76fa54ed07aef2f4574c6e384f2e0f4c8d47d9dd
GuralTOO <guralben@gmail.com>
2024-01-06 00:38:13 -0500

    cleanup

--- src/Documents/SidebarParent.jsx
+++ src/Documents/SidebarParent.jsx
@@ -1,22 +1,14 @@
 
-import React, { useEffect, useState, useCallback } from 'react';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { Folder, Home } from '@mui/icons-material';
-import { NavLink, Outlet } from 'react-router-dom';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import { getFiles } from '../utils/utils';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import '../App.css'
-import { Button } from '@mui/material';
 import Sidebar from './SideBar';
 const SidebarParent = ({userID}) => {
 
-  
   return (
     <div className="grid lg:grid-cols-5">
 
-        {/* render sidebar only if the userID is active */}
         {userID && <Sidebar className="hidden lg:block" style = {{backgroundColor: "black"}}/>}
-
         <div className="border-l col-span-3 lg:col-span-4 lg:border-l">
             <Outlet />
         </div>
